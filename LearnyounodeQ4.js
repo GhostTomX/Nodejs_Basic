3dc0cf8886daef2bffbe0dc7a800ef2d5f05acd7
GhostTomX <r04522839@ntu.edu.tw>
2017-03-09 18:48:14 +0800

    Thired Class  1

---
+++ LearnyounodeQ4.js
@@ -0,0 +1,39 @@
+ const fs = require('fs');
+ var path = process.argv[2];
+while (process.argv[2]){
+fs.readFile(path, (err, data) => {
+        // if (err) throw err;
+        // //console.log(err);
+
+        // var str = data.toString();
+        // var lines = str.split('\n');
+        // console.log(lines.length - 1);
+        console.log('dadada');
+    });
+    process.argv[2]=null;
+    // console.log('dsadsa');
+    // console.log('w');
+    // console.log('ewq');
+    for (var i = 0; i <9999;i++){
+        console.log(i);
+    
+}
+    fs.readFile(path, (err, data) => {
+        // if (err) throw err;
+        // //console.log(err);
+
+        // var str = data.toString();
+        // var lines = str.split('\n');
+        // console.log(lines.length - 1);
+        console.log('qqwqwq');
+    });
+    process.argv[2]=null;
+    // console.log('dsadsa');
+    // console.log('w');
+    // console.log('ewq');
+    for (var i = 0; i <9999;i++){
+        console.log(i);
+    }
+}
+console.log('eeee');
+
